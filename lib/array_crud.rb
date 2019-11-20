d4cb1e922e5d46c69ff0b193c4cb2f578449b4da
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-20 00:47:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  make_a_sentence = ["I", "am", "a", "tall", "girl"]
+  make_a_sentence[1]
 end
 
 def retrieve_first_element_from_array(array)
