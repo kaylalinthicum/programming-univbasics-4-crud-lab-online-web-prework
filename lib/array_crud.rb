dc4c16ce06d2339b86be5c147c9fa443eb498d4f
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-20 00:39:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  nfl_teams = ["Lions", "Bears", "Titans", "Packers", "Giants"]
+  element = "Rams"
+  nfl_teams << "Rams"
 end
 
 def add_element_to_start_of_array(array, element)
