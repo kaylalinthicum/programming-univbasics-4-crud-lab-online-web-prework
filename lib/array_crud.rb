7c8cfa5eb4bf0b3defeed03c361bf51418d57ef0
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-20 00:36:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  mitchell_dogs = ["Stella", "Lita", "Buddy", "Rudy"]
-  mitchell_dogs .push("Titan")
+  
 end
 
 def add_element_to_start_of_array(array, element)
